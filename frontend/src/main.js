1179793a631813a6248c9d122b887d52f3fc00b0
Tato <italoluna23@gmail.com>
2024-05-01 03:29:33 -0500

    feat: added component user register

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,23 +10,27 @@ import InputText from "primevue/inputtext";
 import Sidebar from "primevue/sidebar";
 import Password from 'primevue/password';
 import Card from 'primevue/card';
+import Checkbox from 'primevue/checkbox'
 import i18n from "./locale/i18n.js";
 
 import 'primeflex/primeflex.css';
 import 'primevue/resources/themes/aura-light-green/theme.css'
 import 'primeicons/primeicons.css'
 import 'primevue/resources/primevue.min.css'
+import Divider from "primevue/divider";
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.component('pv-button', Button);
 app.component('pv-toolbar', Toolbar);
 app.component('pv-card', Card);
+app.component('pv-checkbox', Checkbox);
 app.component('pv-password', Password);
 app.component('pv-iconfield', IconField);
 app.component('pv-inputicon', InputIcon);
 app.component('pv-inputtext', InputText);
 app.component('pv-sidebar', Sidebar);
+app.component('pv-divider', Divider);
 app.use(i18n);
 
 app.mount('#app')
---
+++ frontend/src/user/components/the-user-register.component.vue
@@ -0,0 +1,108 @@
+<script>
+export default {
+  name: "the-user-register",
+  data() {
+    return {
+      // userServices: new UserEndpointService(),
+      email: '',
+      username: '',
+      password: '',
+      confirm_password: '',
+      artist: false,
+      password_input_style: {
+        border: 'none',
+        borderBottom: '1px solid #d1d1d1',
+        width: '100%',
+      },
+    };
+  },
+  computed: {
+    //las propiedades Computed son funciones que se ejecutan cada vez que una de las propiedades que utiliza cambia. Lo que permite actualizar el estado de la aplicación en tiempo real.
+    isEmailValid() {
+      const re = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/; //Expresión regular para validar el correo electrónico. Referencia: https://regexr.com/3e48o
+      return re.test(this.email);
+    },
+
+    isPasswordValid() {
+      return this.password.length >= 6 && this.password.length <= 16;
+    },
+
+    isConfirmPasswordValid() {
+      return this.password === this.confirm_password;
+    },
+
+    isUsernameValid() {
+      return this.username.length <= 20 && this.username.length >= 1;
+    },
+
+    isFormValid() {
+      return this.isEmailValid && this.isPasswordValid && this.isConfirmPasswordValid;
+    },
+  },
+}
+</script>
+
+<template>
+  <section class="flex justify-content-center align-items-center" aria-label="Registro de usuario">
+    <div class="register-container pt-7 pb-7">
+      <h4>Registro</h4>
+      <div class="register-form pl-6 pt-4">
+        <div class="flex flex-column gap-3">
+          <label class="uppercase" for="email">CORREO ELECTRÓNICO*</label>
+          <pv-inputtext :invalid="!isEmailValid" v-model="email" class="input-text" id="email" :placeholder="'Ingresa tu correo electrónico'" aria-label="Correo electrónico"/>
+          <small v-if="!isEmailValid" class="text-red-500" aria-label="">Correo electrónico inválido</small>
+
+          <label class="uppercase" for="password">CONTRASEÑA*</label>
+          <pv-password :input-style="password_input_style" :invalid="!isPasswordValid" id="password" toggle-mask :placeholder="'Ingresa tu contraseña (6-16 caracteres)'" v-model="password"
+                       :prompt-label="'Ingresa tu contraseña (6-16 caracteres)'" :weak-label="'Ingresa tu contraseña (6-16 caracteres)'" :medium-label="'Ingresa tu contraseña (6-16 caracteres)'" :strong-label="'Ingresa tu contraseña (6-16 caracteres)'" aria-label="Contraseña"/>
+          <small v-if="!isPasswordValid" class="text-red-500">Contraseña inválida</small>
+
+          <label class="uppercase" for="confirm-password">ESCRIBA NUEVAMENTE LA CONTRASEÑA*</label>
+          <pv-password :input-style="password_input_style" :invalid="!isConfirmPasswordValid" class="password" id="confirm-password" toggle-mask :placeholder="'Ingresa tu contraseña (6-16 caracteres)'" v-model="confirm_password" :feedback="false" aria-label="Confirmar contraseña"/>
+          <small v-if="!isConfirmPasswordValid" class="text-red-500">Ingresa tu contraseña (6-16 caracteres)</small>
+
+          <div class="flex align-items-center">
+            <pv-checkbox v-model="artist" inputId="is_artist" :binary="true" aria-label="Artista"/>
+            <label for="is_artist" class="ml-2"> Al registrarme, acepto los Términos de uso y la Política de privacidad de ArtCollab</label>
+          </div>
+
+          <h4>AlquilaFacil recopila y procesa su dirección de correo electrónico con fines de marketing. Puede darse de baja fácilmente en cualquier momento a través del enlace de exclusión en los correos electrónicos de marketing.</h4>
+
+          <div class="flex justify-content-center gap-2 mt-3 mb-3">
+            <pv-button class="register-button w-full w-12rem" @click="register" :label="'Regístrate ahora'" plain text aria-label="Botón de registro" :disabled="!isFormValid" />
+          </div>
+
+          <pv-divider align="center" type="solid" >
+            <b>Regístrate ahora</b>
+          </pv-divider>
+
+          <div class="flex justify-content-center gap-5">
+            <i class="pi pi-google text-7xl cursor-pointer" aria-label="Google"></i>
+            <i class="pi pi-facebook text-7xl cursor-pointer" aria-label="Facebook"></i>
+            <i class="pi pi-twitter text-7xl cursor-pointer" aria-label="Twitter"></i>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+.register-container {
+  width: 80%;
+}
+
+.register-button{
+  background-color: #c53030;
+}
+
+.input-text{
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid #d1d1d1;
+}
+
+
+</style>
