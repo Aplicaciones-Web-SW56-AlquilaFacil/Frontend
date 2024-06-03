08c8fbc10733bf58524c75401d646a1e4b17e44e
Tato <italoluna23@gmail.com>
2024-06-03 17:09:28 -0500

    fix(form login): update responsive design to form

--- frontend/src/user/components/the-user-login.component.vue
+++ frontend/src/user/components/the-user-login.component.vue
@@ -1,34 +1,38 @@
 <template>
-  <section v-if="showLogin" class="user-login w-full flex justify-content-center align-items-center fixed" @click.self="closeLogin">
-    <pv-card class="login-card h-30rem w-20rem">
+  <section v-if="showLogin" class="user-login flex justify-content-center align-items-center fixed" @click.self="closeLogin">
+    <pv-card class="login-card max-w-md border-round-2xl">
       <template #title>
-        <h4 class="text-center font-bold">Iniciar sesión con correo electrónico</h4>
+        <h4 class="text-center font-bold px-8">Iniciar sesión</h4>
       </template>
       <template #content>
-        <div class="card flex justify-content-center">
-          <div class="flex flex-column gap-1">
-            <label class="text-sm" for="email">Correo electrónico</label>
-            <pv-inputtext class="text-sm" id="email" placeholder="Ingresa tu correo" aria-label="Correo electrónico"/>
+        <div class="card flex justify-content-center w-full">
+          <div class="flex flex-column w-full px-4">
+            <label class="text-sm mb-1" for="email">Correo electrónico</label>
+            <pv-inputtext class="text-sm mb-2 w-full" id="email" placeholder="Ingresa tu correo" aria-label="Correo electrónico"/>
 
-            <label class="text-sm" for="password">Contraseña</label>
-            <pv-password :input-style="{ fontSize: '0.875rem' }" class="text-sm" id="password" toggle-mask v-model="password" placeholder="Ingresar contraseña" aria-label="Contraseña"/>
+            <label class="text-sm mb-1" for="password">Contraseña</label>
+            <pv-inputtext class="text-sm mb-2 w-full" id="password" placeholder="Ingresar contraseña" aria-label="Contraseña">
+              <template #prepend>
+                <i class="pi pi-lock"></i>
+              </template>
+            </pv-inputtext>
 
-            <div class="flex justify-content-center gap-2 mt-3">
-              <pv-button class="w-full bg-white text-sm" label="Atrás" plain text @click="goBack" aria-label="Cerrar inicio de sesión"/>
-              <pv-button class="w-full bg-white text-sm" label="Iniciar sesión" plain text aria-label="Iniciar sesión"/>
+            <div class="flex justify-content-center mt-4 w-full">
+              <pv-button class="styled-button text-sm w-full" label="Iniciar sesión" plain text aria-label="Iniciar sesión"/>
             </div>
+            <router-link to="/forgot-password" class="mt-2">
+              <h5 class="text-center m-1" @click="closeLogin">¿Olvidaste tu contraseña?</h5>
+            </router-link>
           </div>
         </div>
+        <hr class="border-white mt-4">
       </template>
       <template #footer>
-        <div class="flex justify-content-center flex-column">
-          <router-link to="/forgot-password">
-            <h5 class="text-center cursor-pointer" @click="closeLogin">¿Olvidaste tu contraseña?</h5>
-          </router-link>
-          <div class="flex justify-content-center gap-2">
+        <div class="flex justify-content-center flex-column w-full px-5">
+          <div class="flex justify-content-center gap-3 align-items-center w-full">
             <h5 class="text-center">¿No tienes una cuenta?</h5>
             <router-link to="/register">
-              <h5 class="text-center cursor-pointer text-white" @click="closeLogin">Regístrate</h5>
+              <h5 class="text-center text-white" @click="closeLogin">Regístrate</h5>
             </router-link>
           </div>
         </div>
@@ -69,10 +73,32 @@ export default {
   top: 0;
   left: 0;
   z-index: 9999;
-  background-color: rgba(255, 255, 255, 0.7);
+  background-color: rgba(255, 255, 255, 0.5);
 }
 
 .login-card {
   background-color: #c53030;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+
+.styled-button {
+  background-color: #ffffff;
+  color: #c53030;
+  border: 2px solid #c53030;
+  border-radius: 0.5rem;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.styled-button:hover {
+  background-color: darkgray;
+  color: #ffffff;
+}
+
+.text-sm {
+  margin-bottom: 0.5rem; /* Adjusted margin for minimalistic spacing */
 }
 </style>
