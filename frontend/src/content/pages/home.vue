bc54239f6192f6fba008c2e7936395760a6eaf70
Augusto Pin <augustopm607@gmail.com>
2024-06-04 01:17:20 -0500

    feat: add search bar to home page

--- frontend/src/content/pages/home.vue
+++ frontend/src/content/pages/home.vue
@@ -8,6 +8,21 @@ export default defineComponent({
 </script>
 
 <template>
+
+  <div class="search-container">
+      <div class="search-bar">
+        <div class="search-text-column">
+          <label for="search" class="search-label">Donde</label>
+          <div class="search-input-container">
+          <input type="text" id="search" class="search-input" v-model="query" placeholder="Solo ingresa el lugar" />
+          </div>
+        </div>
+        <button @click="search" class="search-button">
+          <i class="pi pi-search"></i>
+        </button>
+      </div>
+  </div>
+
   <div class="container">
     <div class="grid">
       <the-home class="home-card"></the-home>
@@ -16,6 +31,71 @@ export default defineComponent({
 </template>
 
 <style>
+.search-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.search-text-column {
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.search-bar {
+  display: flex;
+  border: 2px solid #D13333;
+  border-radius: 50px;
+  padding: 10px;
+  width: 20%;
+  background-color: white;
+}
+
+.search-label {
+  margin-left: 25px;
+  font-weight: bold;
+}
+
+.search-input {
+  border: none;
+  outline: none;
+  padding: 5px;
+  margin-left: 20px;
+  flex: 1;
+  border-radius: 4px;
+}
+
+.search-input-container {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.search-button {
+  background-color: #D13333;
+  border: none;
+  border-radius: 50%;
+  color: white;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin-left: 30%;
+}
+
+.search-button:hover {
+  background-color: #c12e2e;
+}
+
+.search-button i {
+  font-size: 1.2em;
+}
+
+
+
+
 .container {
   display: flex;
   justify-content: center;
